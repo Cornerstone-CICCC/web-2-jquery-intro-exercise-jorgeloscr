8e3e50b45cdcc9712355508142343bca6b515c51
Jorge Ortega <jorgeloscr@hotmail.com>
2024-07-29 17:31:34 -0700

    Exercise Completed

--- app.js
+++ app.js
@@ -1,3 +1,36 @@
 $(function() {
   // YOUR CODE HERE
+
+    $('#toggleButton').click(function() {
+        $('#myParagraph').toggle();
+    });
+
+    $('#colorButton').click(function() {
+        $('#colorDiv').css('background-color', 'red');
+    });
+
+    $('#addClassButton').click(function() {
+        $('#classDiv').addClass('highlight');
+    });
+
+    $('#removeClassButton').click(function() {
+        $('#classDiv').removeClass('highlight');
+    });
+
+    $('#fadeInButton').click(function() {
+        $('#fadeDiv').fadeIn();
+    });
+
+    $('#fadeOutButton').click(function() {
+        $('#fadeDiv').fadeOut();
+    });
+
+    $('#slideUpButton').click(function() {
+        $('#slideDiv').slideUp();
+    });
+
+    $('#slideDownButton').click(function() {
+        $('#slideDiv').slideDown();
+    });
+
 })
